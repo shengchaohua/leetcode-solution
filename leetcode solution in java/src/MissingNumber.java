7b4e0357d5786f457d06c5c778f75158cb0d4d7d
shengchaohua <shengchaohua163@163.com>
2018-03-03 14:16:23 +0800

    modify No.268 MissingNumber.java

--- leetcode solution in java/src/MissingNumber.java
+++ leetcode solution in java/src/MissingNumber.java
@@ -2,9 +2,9 @@ public class MissingNumber {
 	
 	public int missingNumber(int[] nums) {
 		int len = nums.length;
-	    int sum = (0+len)*(len+1)/2;
-	    for(int i=0; i<len; i++)
-	        sum-=nums[i];
+	    int sum = (0+len) * (len+1) / 2;
+	    for(int i = 0; i < len; i++)
+	        sum -= nums[i];
 	    return sum;
     }
 	
