f0887df820752173f95dc3f19ba02f8e5efeec43
shengchaohua <shengchaohua163@163.com>
2018-03-10 10:15:26 +0800

    add No.217 Contains Duplicate

---
+++ leetcode solution in python/ContainsDuplicate.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 10 10:12:39 2018
+
+@problem: Leetcode No.217 Contains Duplicate
+
+@author: shengchaohua
+"""
+
+class ContainsDuplicate:
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(nums) != len(set(nums))
+    
+def main():
+    cd = ContainsDuplicate()
+    print(cd.containsDuplicate([1,2,3,4,4]))
+    
+if __name__ == '__main__':
+    main()
