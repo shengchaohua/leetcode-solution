a23e0df4b0c87a30a6ea1d06f58b7f282f848ec4
shengchaohua <shengchaohua163@163.com>
2018-03-13 21:19:50 +0800

    No.697 Degree of an Array

---
+++ leetcode solution in java/src/DegreeOfAnArray.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DegreeOfAnArray {
+	public int findShortestSubArray(int[] nums) {
+        Map<Integer, Integer> numFreq = new HashMap<>();
+        for (int num : nums) {
+        	numFreq.put(num, numFreq.getOrDefault(num, 0)+1);
+        }
+        
+        int maxFreq = 0;
+        List<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> item : numFreq.entrySet()) {
+        	if (item.getValue() > maxFreq) {
+        		list.clear();
+        		list.add(item.getKey());
+        		maxFreq = item.getValue();
+        	} else if (item.getValue() == maxFreq) {
+        		list.add(item.getKey());
+        	}
+        }
+        
+        int smallertLen = Integer.MAX_VALUE;
+        for (int i = 0; i < list.size(); i++) {
+        	int mostFreqNum = list.get(i); 
+	        int low = 0;
+	    	int high = nums.length-1;
+	    	
+	        while (nums[low] != mostFreqNum)
+	        	low++;
+	        while (nums[high] != mostFreqNum)
+	        	high--;
+	        int len = high-low+1;
+	        smallertLen =  len < smallertLen ? len : smallertLen;
+        }
+        return smallertLen;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = {1,2,2,3,1,4,2};
+		DegreeOfAnArray de = new DegreeOfAnArray();
+		System.out.println(de.findShortestSubArray(nums));
+	}
+
+}
