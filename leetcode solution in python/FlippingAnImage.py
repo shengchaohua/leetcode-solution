704908fb3663a2191c611eae9eb8d4d4c72fab15
shengchaohua <shengchaohua163@163.com>
2018-08-24 21:53:22 +0800

    add leetcode No.832 Flipping an Image

---
+++ leetcode solution in python/FlippingAnImage.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 24 21:40:03 2018
+
+@problem: leetcode No.832 Flipping an Image
+
+@author: shengchaohua
+"""
+
+class Solution:
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        res = []
+        for row in A:
+            row.reverse()
+            res.append([1-x for x in row])
+        return res
+
+def main():
+    s = Solution()
+    A = [[1,1,0],[1,0,1],[0,0,0]]
+    print(s.flipAndInvertImage(A))
+    
+if __name__ == '__main__':
+    main()
