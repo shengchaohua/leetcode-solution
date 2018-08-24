3393e34511b02e8cb874e83be65790a8204097fb
shengchaohua <shengchaohua163@163.com>
2018-08-24 21:18:51 +0800

    add leetcode No.709 To Lower Case

---
+++ leetcode solution in python/ToLowerCase.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 24 21:12:05 2018
+
+@problem: leetcode No.709 To Lower Case
+
+@author: shengchaohua
+"""
+
+class Solution:
+    def toLowerCase(self, str1):
+        """
+        :type str: str
+        :rtype: str
+        """
+        return str1.lower()
