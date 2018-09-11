bd95262d870602607f39d1804e44191986e409df
shengchaohua <shengchaohua163@163.com>
2018-09-11 20:12:42 +0800

    add leetcode m = len(matrix)

---
+++ leetcode solution in python/ToeplitzMatrix.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 11 20:04:31 2018
+
+@problem: leetcode 766. Toeplitz Matrix
+
+@author: shengchaohua
+"""
+
+class Solution:
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        m = len(matrix)
+        for i in range(m-1):
+            if matrix[i][0:-1] != matrix[i+1][1:]:
+                return False
+        return True
+    
+def main():
+    s = Solution()
+    matrix = [
+      [1,2,3,4],
+      [5,1,2,3],
+      [9,5,1,2]
+    ]
+    print(s.isToeplitzMatrix(matrix))
+    
+if __name__ == '__main__':
+    main()
+            
