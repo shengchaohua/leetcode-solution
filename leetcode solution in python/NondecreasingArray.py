dfbd4696c21ee55712c90beede71d309bdc553a7
shengchaohua <shengchaohua163@163.com>
2018-12-26 10:54:45 +0800

    modify leetcode 665. Non-decreasing Array

--- leetcode solution in python/NondecreasingArray.py
+++ leetcode solution in python/NondecreasingArray.py
@@ -13,25 +13,41 @@ class Solution:
         :type nums: List[int]
         :rtype: bool
         """
-        i = 0
-        while i < len(nums)-1 and nums[i] <= nums[i+1]:
-            i += 1
-        
-        if i == len(nums)-1:
+        # Brute solution. TLE!
+        def monotone_increasing(arr):
+            for i in range(len(arr) - 1):
+                if arr[i] > arr[i+1]:
+                    return False
             return True
-#        if i > 0 and i < len(nums) and 
-        else:
-            nums[i] = nums[i+1]
-            while i < len(nums)-1 and nums[i] <= nums[i+1]:
-                i += 1 
-        
-            return i == len(nums)-1
+
+        new = nums[:]
+        for i in range(len(nums)):
+            old_ai = nums[i]
+            new[i] = new[i-1] if i > 0 else float('-inf')
+            if monotone_increasing(new):
+                return True
+            new[i] = old_ai
+
+        return False
+    
+    def checkPossibility2(self, nums):
+        # Locate and Analyze Problem Index
+        p = None
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i+1]:
+                if p is not None:
+                    return False
+                p = i
+
+        return (p is None or p == 0 or p == len(nums)-2 or
+                nums[p-1] <= nums[p+1] or nums[p] <= nums[p+2]) 
     
 
 def test():
     s = Solution()
     nums = [3,4,2,3]
     print(s.checkPossibility(nums))
+    print(s.checkPossibility2(nums))
 
 
 if __name__ == '__main__':
